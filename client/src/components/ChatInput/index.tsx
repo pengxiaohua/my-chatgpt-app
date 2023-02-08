af556b9d396825aaf40cf820191ff999d3910375
Xiaohua Peng <609370075@qq.com>
2023-02-08 15:25:18 +0800

    feat: add client folder

--- client/src/components/ChatInput/index.tsx
+++ client/src/components/ChatInput/index.tsx
@@ -1,11 +1,13 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { Input } from 'antd';
 
 import './index.scss'
 
 const { Search } = Input;
 
-interface IProps { }
+interface IProps {
+    getSearchVal: (val: string) => void
+}
 
 const defaultProps = {}
 
@@ -14,18 +16,29 @@ type DefaultProps = Readonly<typeof defaultProps>
 type Props = IProps & Partial<DefaultProps>
 
 const ChatInput: FC<Props> = (props) => {
+    const { getSearchVal } = props
+
+    const [searchVal, setSearchVal] = useState('')
 
     const onSearch = (text: string) => {
-        console.log({ text });
-
+        getSearchVal(text)
+        setSearchVal('')
     }
+
+    const handleSearchChange = (e: React.ChangeEvent) => {
+        const {target: { value }} = e
+        setSearchVal(value)
+    }
+
     return (
         <div className="chat-input">
             <Search
+                value={searchVal}
                 placeholder="input search text"
                 onSearch={onSearch}
                 enterButton
                 className="chat-input__wrap"
+                onChange={handleSearchChange}
                 allowClear
             />
         </div>
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react"
+import { FC, useState } from "react"
+import cloneDeep from "lodash/cloneDeep"
+import { UserOutlined, RobotOutlined } from '@ant-design/icons'
 
 import ChatInput from "../../components/ChatInput"
 
@@ -13,12 +15,20 @@ type DefaultProps = Readonly<typeof defaultProps>
 type Props = IProps & Partial<DefaultProps>
 
 const Home: FC<Props> = (props) => {
+  const [valList, setValList] = useState([] as string[])
+
+  const handleSearchVal = (val: string) => {
+    const currentValList = cloneDeep(valList)
+    currentValList.push(val)
+    setValList(currentValList)
+  }
+
   return (
     <div className="home">
       <div className="home__container">
         
       </div>
-      <ChatInput />
+      <ChatInput getSearchVal={handleSearchVal} />
     </div>
   )
 }
