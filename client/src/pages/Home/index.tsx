5b7d542a02fadaf215ea128efbe53b083177b564
Xiaohua Peng <609370075@qq.com>
2023-02-10 16:40:16 +0800

    feat: 增加数据展示样式

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
-import { FC, useState } from "react"
+import { FC, useState, useEffect } from "react"
 import cloneDeep from "lodash/cloneDeep"
-import { UserOutlined, RobotOutlined } from '@ant-design/icons'
+import { UserOutlined, RobotOutlined, LoadingOutlined } from '@ant-design/icons'
 
 import ChatInput from "../../components/ChatInput"
 
@@ -16,6 +16,23 @@ type Props = IProps & Partial<DefaultProps>
 
 const Home: FC<Props> = (props) => {
   const [valList, setValList] = useState([] as string[])
+  const [answerLoader, setAnswerLoader] = useState('')
+
+  useEffect(() => {
+    let newLoader = cloneDeep(answerLoader)
+
+    const timer = setInterval(() => {
+      newLoader += '.'
+      setAnswerLoader(newLoader)
+
+      if (newLoader === '....') {
+        setAnswerLoader('')
+      }
+    }, 300);
+
+    return () => clearInterval(timer)
+
+  }, [answerLoader])
 
   const handleSearchVal = (val: string) => {
     const currentValList = cloneDeep(valList)
@@ -23,10 +40,27 @@ const Home: FC<Props> = (props) => {
     setValList(currentValList)
   }
 
+  const renderValItem = (item: string) => (
+    <div className="home__val-item" key={`${item}-${Date.now()}`}>
+      <div className="home__val-item__question">
+        <UserOutlined className="home__val-item__question__avatar" />
+        <div className="home__val-item__val">{item}</div>
+      </div>
+      <div className="home__val-item__answer">
+        <RobotOutlined className="home__val-item__answer__avatar"/>
+        <div className="home__val-item__val">
+          {answerLoader}
+        </div>
+      </div>
+    </div>
+  )
+
   return (
     <div className="home">
-      <div className="home__container">
-        
+      <div className="home__val-list">
+        {
+          valList.length > 0 && valList.map(renderValItem)
+        }
       </div>
       <ChatInput getSearchVal={handleSearchVal} />
     </div>
