be123dda2bb6c05d81fc2260b6f109c56ac7f661
Xiaohua Peng <609370075@qq.com>
2023-02-14 08:59:54 +0800

    feat: update chatgpt logo icon

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,44 +1,47 @@
-import { FC, useState, useEffect } from "react"
-import cloneDeep from "lodash/cloneDeep"
-import { UserOutlined, RobotOutlined, LoadingOutlined } from '@ant-design/icons'
+import { FC, useState, useEffect } from "react";
+import cloneDeep from "lodash/cloneDeep";
+import {
+  UserOutlined,
+  RobotOutlined,
+  LoadingOutlined,
+} from "@ant-design/icons";
 
-import ChatInput from "../../components/ChatInput"
+import ChatInput from "../../components/ChatInput";
 
-import './index.scss'
+import "./index.scss";
 
 interface IProps {}
 
-const defaultProps = {}
+const defaultProps = {};
 
-type DefaultProps = Readonly<typeof defaultProps>
+type DefaultProps = Readonly<typeof defaultProps>;
 
-type Props = IProps & Partial<DefaultProps>
+type Props = IProps & Partial<DefaultProps>;
 
 const Home: FC<Props> = (props) => {
-  const [valList, setValList] = useState([] as string[])
-  const [answerLoader, setAnswerLoader] = useState('')
+  const [valList, setValList] = useState([] as string[]);
+  const [answerLoader, setAnswerLoader] = useState("");
 
   useEffect(() => {
-    let newLoader = cloneDeep(answerLoader)
+    let newLoader = cloneDeep(answerLoader);
 
     const timer = setInterval(() => {
-      newLoader += '.'
-      setAnswerLoader(newLoader)
+      newLoader += ".";
+      setAnswerLoader(newLoader);
 
-      if (newLoader === '....') {
-        setAnswerLoader('')
+      if (newLoader === "....") {
+        setAnswerLoader("");
       }
     }, 300);
 
-    return () => clearInterval(timer)
-
-  }, [answerLoader])
+    return () => clearInterval(timer);
+  }, [answerLoader]);
 
   const handleSearchVal = (val: string) => {
-    const currentValList = cloneDeep(valList)
-    currentValList.push(val)
-    setValList(currentValList)
-  }
+    const currentValList = cloneDeep(valList);
+    currentValList.push(val);
+    setValList(currentValList);
+  };
 
   const renderValItem = (item: string) => (
     <div className="home__val-item" key={`${item}-${Date.now()}`}>
@@ -47,26 +50,44 @@ const Home: FC<Props> = (props) => {
         <div className="home__val-item__val">{item}</div>
       </div>
       <div className="home__val-item__answer">
-        <RobotOutlined className="home__val-item__answer__avatar"/>
-        <div className="home__val-item__val">
-          {answerLoader}
+        {/* <RobotOutlined className="home__val-item__answer__avatar"/> */}
+        <div className="home__val-item__answer__avatar">
+          <svg
+            data-name="OpenAI Logo"
+            width="32"
+            height="32"
+            viewBox="140 140 520 520">
+            <defs>
+              <linearGradient id="linear" x1="100%" y1="22%" x2="0%" y2="78%">
+                <stop offset="0%" stop-color="rgb(131,211,231)"></stop>
+                <stop offset="2%" stop-color="rgb(127,203,229)"></stop>
+                <stop offset="25%" stop-color="rgb(86,115,217)"></stop>
+                <stop offset="49%" stop-color="rgb(105,80,190)"></stop>
+                <stop offset="98%" stop-color="rgb(197,59,119)"></stop>
+                <stop offset="100%" stop-color="rgb(197,59,119)"></stop>
+              </linearGradient>
+            </defs>
+            <path
+              id="logo"
+              d="m617.24 354a126.36 126.36 0 0 0 -10.86-103.79 127.8 127.8 0 0 0 -137.65-61.32 126.36 126.36 0 0 0 -95.31-42.49 127.81 127.81 0 0 0 -121.92 88.49 126.4 126.4 0 0 0 -84.5 61.3 127.82 127.82 0 0 0 15.72 149.86 126.36 126.36 0 0 0 10.86 103.79 127.81 127.81 0 0 0 137.65 61.32 126.36 126.36 0 0 0 95.31 42.49 127.81 127.81 0 0 0 121.96-88.54 126.4 126.4 0 0 0 84.5-61.3 127.82 127.82 0 0 0 -15.76-149.81zm-190.66 266.49a94.79 94.79 0 0 1 -60.85-22c.77-.42 2.12-1.16 3-1.7l101-58.34a16.42 16.42 0 0 0 8.3-14.37v-142.39l42.69 24.65a1.52 1.52 0 0 1 .83 1.17v117.92a95.18 95.18 0 0 1 -94.97 95.06zm-204.24-87.23a94.74 94.74 0 0 1 -11.34-63.7c.75.45 2.06 1.25 3 1.79l101 58.34a16.44 16.44 0 0 0 16.59 0l123.31-71.2v49.3a1.53 1.53 0 0 1 -.61 1.31l-102.1 58.95a95.16 95.16 0 0 1 -129.85-34.79zm-26.57-220.49a94.71 94.71 0 0 1 49.48-41.68c0 .87-.05 2.41-.05 3.48v116.68a16.41 16.41 0 0 0 8.29 14.36l123.31 71.19-42.69 24.65a1.53 1.53 0 0 1 -1.44.13l-102.11-59a95.16 95.16 0 0 1 -34.79-129.81zm350.74 81.62-123.31-71.2 42.69-24.64a1.53 1.53 0 0 1 1.44-.13l102.11 58.95a95.08 95.08 0 0 1 -14.69 171.55c0-.88 0-2.42 0-3.49v-116.68a16.4 16.4 0 0 0 -8.24-14.36zm42.49-63.95c-.75-.46-2.06-1.25-3-1.79l-101-58.34a16.46 16.46 0 0 0 -16.59 0l-123.31 71.2v-49.3a1.53 1.53 0 0 1 .61-1.31l102.1-58.9a95.07 95.07 0 0 1 141.19 98.44zm-267.11 87.87-42.7-24.65a1.52 1.52 0 0 1 -.83-1.17v-117.92a95.07 95.07 0 0 1 155.9-73c-.77.42-2.11 1.16-3 1.7l-101 58.34a16.41 16.41 0 0 0 -8.3 14.36zm23.19-50 54.92-31.72 54.92 31.7v63.42l-54.92 31.7-54.92-31.7z"
+              fill="#FFFFFF"></path>
+          </svg>
         </div>
+        <div className="home__val-item__val">{answerLoader}</div>
       </div>
     </div>
-  )
+  );
 
   return (
     <div className="home">
       <div className="home__val-list">
-        {
-          valList.length > 0 && valList.map(renderValItem)
-        }
+        {valList.length > 0 && valList.map(renderValItem)}
       </div>
       <ChatInput getSearchVal={handleSearchVal} />
     </div>
-  )
-}
+  );
+};
 
-Home.defaultProps = defaultProps
+Home.defaultProps = defaultProps;
 
-export default Home
+export default Home;
